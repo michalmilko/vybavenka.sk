022f61b59a42001ea390425a8aa4389059d95216
Michal <tcom.milko@gmail.com>
2023-04-24 15:13:23 +0200

    nastavenie backend s forntend

--- src/components/Menu/AddBook/AddBook.js
+++ src/components/Menu/AddBook/AddBook.js
@@ -3,44 +3,42 @@ import axios from 'axios';
 import "./AddBook.css";
 
 const Add = () => {
-  const [Nazov, setText1] = useState('');
+  //UKLADANIE DAT Z INPUT
+  const [MenoPriezvisko, setMenoPriezvisko] = useState('');
+  const [Nazov, setNazov] = useState('');
+  const [Kategoria, setKategoria] = useState('');
+  const [Kvalita, setKvalita] = useState('');
   const [Image, setFile] = useState(null);
-  const [Kategoria, setOption1] = useState('');
-  const [Kvalita, setOption2] = useState('');
-  const [Popis, setText2] = useState('');
+  const [Popis, setPopis] = useState('');
+  const [Email, setEmail] = useState ('');
+  const [deletePassword, setPassword]= useState ("");
 
   const handleSubmit = async (event) => {
-  //  event.preventDefault();
+   event.preventDefault();
 
+  //VYTVORENIE OBJEKTU DAT
   const formData = new FormData();
+formData.append('MenoPriezvisko', MenoPriezvisko);
 formData.append('Nazov', Nazov);
 formData.append('Image', Image);
 formData.append('Kategoria', Kategoria);
 formData.append('Kvalita', Kvalita);
 formData.append('Popis', Popis);
+formData.append('Email', Email);
+formData.append('deletePassword', deletePassword);
 
+
+//POSLANIE DAT DO DB
 axios.post('https://endpoint.vybavenka.sk/api/users', formData)
   .then((res) => console.log(res.data))
   .catch((err) => console.log(err));
-
-  
-    // setText1('');
-    // setFile(null);
-    // setOption1('');
-    // setOption2('');
-    // setText2('');
- 
-  
-
  };
 
 
   return (
     <div className='fixedCeleCancel'>
     <div className="addCelyObsah">
-   
     <form onSubmit={handleSubmit}>
-      
        <div>
       <h2 className="addNadpisCely">PRIDAJ KNIHU NA PREDAJ</h2>
       </div>
@@ -48,14 +46,14 @@ axios.post('https://endpoint.vybavenka.sk/api/users', formData)
       <label>
       <h5>Nazov Knihy</h5>
         <input className="addNazov" placeholder="Nazov" type="text" required minLength={2} maxLength={25} value={Nazov}
-       onChange={(event) => setText1(event.target.value)} />
+       onChange={(event) => setNazov(event.target.value)} />
   
       </label>
       </div>
       <div className='addKategoriaKvalita'>
       <label>
       <h5>Kategoria</h5>
-        <select className="addKategoria" value={Kategoria} required onChange={(event) => setOption1(event.target.value)}>
+        <select className="addKategoria" value={Kategoria} required onChange={(event) => setKategoria(event.target.value)}>
           <option value=""></option>
           <option value={"Školske"}>Školske</option>
          <option value={"Roman"}>Román</option>
@@ -66,7 +64,7 @@ axios.post('https://endpoint.vybavenka.sk/api/users', formData)
       </label>
       <label>
       <h5>Kvalita</h5>
-        <select className="addKvalita" value={Kvalita} required onChange={(event) => setOption2(event.target.value)}>
+        <select className="addKvalita" value={Kvalita} required onChange={(event) => setKvalita(event.target.value)}>
           <option value=""></option>
           <option value={"100%"} >100%</option>
          <option value={"80%"}>80%</option>
@@ -80,16 +78,26 @@ axios.post('https://endpoint.vybavenka.sk/api/users', formData)
       <div className='addFileUpload'>
       <label>
         <h5>Nahrať knihu</h5>
-      
         <input className="addFile" type="file" required accept=".png, .jpg, .jpeg" onChange={(event) => setFile(event.target.files[0])} />
       </label>
       </div>
       <div className='addCenaPredaja'>
       <label>
       <h5>Cena predaja</h5>
-        <input className="addCena"  type="number" min="0" required placeholder="Cena" value={Popis} onChange={(event) => setText2(event.target.value)} />
+        <input className="addCena"  type="number" min="0" required placeholder="Cena" value={Popis} onChange={(event) => setPopis(event.target.value)} />
       </label>
       </div>
+      <div className="menoPasswordEmail">
+      <h5>Kontaktne udaje</h5>
+      <input className="menoPriezvisko" type="text" placeholder="Meno" value={MenoPriezvisko}
+       onChange={(event) => setMenoPriezvisko(event.target.value)} />
+       <input className="Email" placeholder="E-mail" type="email" required minLength={2} maxLength={35} value={Email}
+       onChange={(event) => setEmail(event.target.value)} />
+       <h5>Heslo na zmazanie inzeratu</h5>
+       `<input className="Password" placeholder="Heslo" type="password" required minLength={2} maxLength={25} value={deletePassword}
+       onChange={(event) => setPassword(event.target.value)} />`
+      </div>
+
       <div className='addButton'>
       <button className="addSubmit"  type="submit">Pridat Knihu</button>
       </div>
