1c233cc3b17186f293fda01a5e89cb04549266d1
Michal <tcom.milko@gmail.com>
2023-04-29 20:36:01 +0200

    Pridanie backend delete inzeratu

--- frontend/src/components/Box/BoxProfil/BoxProfil.js
+++ frontend/src/components/Box/BoxProfil/BoxProfil.js
@@ -1,25 +1,44 @@
 import "./BoxProfil.css"
 import BoxMenuProfil from "./BoxProfilMenuObsah/BoxMenuProfil"
 import BoxObsahProfil from "./BoxProfilMenuObsah/BoxObsahProfil"
+import axios from 'axios';
+import { useState } from "react";
 
-const BoxProfil = (props) =>{
-
-   
-   return(
-      <div className="boxProfil">
-         {/* Zobrazenie v Menu inzeratu Profil */}
-         <div className="boxMenuMenu">
-         <BoxMenuProfil PresunPonuka={props.PresunPonuka}/>
-         </div>
-         {/* Zobrazit kontakte udaje v Profile */}
-         <div className="boxFullProfil">
-         <BoxObsahProfil prezivkaUser={props.prezivkaUser} userEmail={props.userEmail}/>
-         </div>
-         {/* Vymazanie inzeratu v Profile */}
-         <div className="boxDeleteProfil">
-         <button className="boxDeleteButton">Vymazať Inzerat</button>
-         </div>
-      </div>
-   )
-}
-export default BoxProfil
+const BoxProfil = (props) => {
+   const [id, setId] = useState(props.id);
+ 
+   const buttonDelete = async () => {
+     try {
+       console.log(id);
+       await axios.delete(`https://endpoint.vybavenka.sk/api/users/${id}`, {
+             data: {
+               id: id
+                 }
+            });
+       setId();
+       window.location.reload();
+     } catch (error) {
+       console.log(error);
+     }
+   };
+ 
+   return (
+     <div className="boxProfil">
+       {/* Zobrazenie v Menu inzeratu Profil */}
+       <div className="boxMenuMenu">
+         <BoxMenuProfil PresunPonuka={props.PresunPonuka} />
+       </div>
+       {/* Zobrazit kontakte udaje v Profile */}
+       <div className="boxFullProfil">
+         <BoxObsahProfil prezivkaUser={props.prezivkaUser} userEmail={props.userEmail} />
+       </div>
+       {/* Vymazanie inzeratu v Profile */}
+       <div className="boxDeleteProfil">
+         <button className="boxDeleteButton" onClick={buttonDelete}>Vymazať Inzerat</button>
+       </div>
+     </div>
+   );
+ };
+ 
+ export default BoxProfil;
+ 
--- server/Index.js
+++ server/Index.js
@@ -6,13 +6,15 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+
+//Pristupy na databazu
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'vybavenk_vybavenka',
   password: 'WrZ2BA7HTw5*',
   database: 'vybavenk_book'
 });
-
+//Pripojenie na databazu
 connection.connect((err) => {
   if (err) throw err;
   console.log('MySQL database connected!');
@@ -31,34 +33,45 @@ const storage = multer.diskStorage({
 
 
 const upload = multer({ storage: storage });
-
 const router = express.Router();
-
+//Poslat data
 router.post('/users', upload.single('Image'), (req, res) => {
-
+//data z formulara
   const {MenoPriezvisko, Nazov, Kategoria, Kvalita, Popis, Email, deletePassword} = req.body;
   const filename = path.basename(req.file.filename);
-
+//sql kod
   const sql = 'INSERT INTO bookusers (MenoPriezvisko, Nazov, Image, Kategoria, Kvalita, Popis, Email, deletePassword) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
    const values = [MenoPriezvisko, Nazov, filename, Kategoria, Kvalita, Popis, Email, deletePassword];
-
-  
+  //pripojit a poslat
   connection.query(sql, values, (error, result) => {
     if (error) throw error;
     console.log(result);
-    res.send('User added to database!');
+    res.send('Uspesne pridane data!');
   });
 });
-
+//Dostat data 
 router.get('/users', (req, res) => {
+  //pripojit a zobrazit
   connection.query('SELECT * FROM bookusers', (err, rows) => {
     if (err) throw err;
     res.send(rows);
   });
 });
-
+//mazanie dat
+router.delete('/users/:id', (req, res) => {
+  const id = req.params.id;
+  //sql kod
+  const sqldelete = 'DELETE FROM bookusers WHERE id = ?';
+  //pripojenie a zmazanie
+  connection.query(sqldelete, id, (error, result) => {
+    if (error) throw error;
+    console.log(result);
+    res.send('Záznam bol úspešne vymazaný');
+  });
+});
+//router na api/
 app.use('/api', router);
-
+//pocuva na porte
 app.listen(port, () => {
   console.log('Server bude spustený na porte ' + port);
 });
